84e89e48bfc6154b79f3eebcaf7d4ea59b02a2ad
MugenAkmal <md.akmal260896@gmail.com>
2022-02-16 03:02:29 +0800

    tutorial done from javasciprt mastery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+import { getPlacesData, getWeatherData } from './api/index';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [places, setPlaces] = useState([]);
+    const [weather, setWeatherData] = useState([]);
+    const [filterPlaces, setFilterPlaces] = useState([]);
+    const [coordinates, setCoordinates] = useState({lat: 0, lng:0});
+    const [bound, setBounds] = useState({});
+    const [childClicked, setChildClicked] =  useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [type, setType] = useState('restaurant');
+    const [rating, setRating] = useState('');
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
+            setCoordinates({lat:latitude, lng:longitude});
+        })
+    }, []);
+
+    useEffect(() => {
+        const filterPlaces = places.filter((place) => place.rating >rating);
+        setFilterPlaces(filterPlaces);
+    }, [rating])
+
+    useEffect(() => {
+        try {
+            if(bound.sw && bound.ne){
+                setIsLoading(true);
+                getWeatherData(coordinates.lat, coordinates.lng)
+                    .then((data) => setWeatherData(data));
+                getPlacesData(type, bound.sw, bound.ne)
+                    .then((data) => {
+                    setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+                    console.log('dataaaa',data);
+                    setFilterPlaces([]);
+                    setIsLoading(false);
+                });
+            }
+        } catch(e) {
+            console.log('ERRRRROR!!! : ', e);
+        }
+    }, [type, bound]);
+
+    return (
+        <div>
+            <CssBaseline />
+            <Header setCoordinates={setCoordinates} />
+            <Grid container spacing={3} style={{ width: '100%'}}>
+                <Grid item xs={12} md={4}>
+                    <List 
+                    places={filterPlaces.length ? filterPlaces : places} 
+                    childClicked={childClicked} 
+                    isLoading={isLoading}
+                    type={type}
+                    setType={setType}
+                    rating={rating}
+                    setRating={setRating}
+                    />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                        places={filterPlaces.length ? filterPlaces : places}
+                        setChildClicked={setChildClicked}
+                        weather={weather}
+                    />
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/List/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1), minWidth: 120, marginBottom: '30px',
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+    loading: {
+        height: '60px', display: 'flex', justifyContent: 'center', alignItems: 'center',
+    },
+    container: {
+        padding: '25px',
+    },
+    list: {
+        height: '75vh', overflow: 'auto',
+    }
+}));
